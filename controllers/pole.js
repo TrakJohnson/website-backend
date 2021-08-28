b9b8368726b9ff4243850158366fa43ec87dfe96
Antoine Ser <antoine.sherwood99@gmail.com>
2021-08-28 10:35:12 +0200

    see team in one request

--- controllers/pole.js
+++ controllers/pole.js
@@ -1,5 +1,6 @@
 
 const funcs = require('../functions/functions');
+const Member = require('../models/member');
 const Pole = require('../models/pole');
 
 exports.getPoles = (req, res, next) => {
@@ -12,9 +13,14 @@ exports.getPoles = (req, res, next) => {
             const getData = async () => {
                 for (var i = 0; i < data.length; i++) {
                     var poleData = data[i];
-                    poleData.members = await funcs.bddQuery(req.conBDA, "SELECT * FROM newMembers WHERE pole_id = ?", [poleData.pole_id]);
+                    poleData.members = await funcs.bddQuery(req.conBDA, "SELECT * FROM newMembers JOIN newUsers ON newMembers.login = newUsers.login WHERE pole_id = ?", [poleData.pole_id]);
+                    for (var j = 0; j < poleData.members.length; j++) {
+                        poleData.members[j] = new Member(poleData.members[j]);
+                        console.log({member : poleData.members[j] });
+                    }
+                    console.log({pole: new Pole(poleData)});
                     polesToSendToFrond.push(new Pole(poleData));
-                }
+                }j
             }
             await getData();
             funcs.sendSuccess(res, polesToSendToFrond);
