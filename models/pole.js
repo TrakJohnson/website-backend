d049d762591df5f93165ed842de99792bc2ae7aa
Antoine Ser <antoine.sherwood99@gmail.com>
2021-08-28 21:56:38 +0200

    added color to pole

--- models/pole.js
+++ models/pole.js
@@ -5,6 +5,7 @@ var Pole = class Pole {
         this.pole_id = data.pole_id;
         this.name = data.name;
         this.description = data.description;
+        this.color = data.color;
         this.members = data.members;
     }  
 
@@ -12,6 +13,7 @@ var Pole = class Pole {
         this.pole_id = newInfos.pole_id != undefined ? newInfos.pole_id : this.pole_id;
         this.name = newInfos.name != undefined ? newInfos.name : this.name;
         this.description = newInfos.description != undefined ? newInfos.description : this.description;
+        this.color = newInfos.color != undefined ? newInfos.color : this.color;
         this.members = newInfos.members != undefined ? newInfos.members : this.members;
     }
 }
