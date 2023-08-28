460f6c88a81845ff01cd44a21515f886d2e7e09e
Arthur Louvrier <arthur.louvrier@etu.minesparis.psl.eu>
2023-08-28 18:33:34 +0200

    Added script to automatically upload the code to the server

---
+++ serverUpload.bat
@@ -0,0 +1,10 @@
+echo off
+setlocal
+set files="./controllers ./functions ./middlewares ./models ./routes ./index.js package.json package-lock.json router.js"
+
+echo Sending %files%
+
+pscp -r -i .\cleBDA.ppk "%files%" ec2-user@ec2-54-166-175-189.compute-1.amazonaws.com:/home/ec2-user
+
+endlocal
+echo "Don't forget to upload the .env file and to restart the servers with: pm2 restart 0"
